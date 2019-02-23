b97464bd9388c8d90de0f50883063297f532bc30
Sung Kim <sungmkim80@gmail.com>
2019-02-23 01:11:49 -0500

    --
    added source code from https://github.com/gatsbyjs/gatsby/blob/master/examples/using-wordpress/src/components/post-icons.js but not yet working due to ACF stuff...

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,53 +36,15 @@ module.exports = {
     {
       resolve: "gatsby-source-wordpress",
       options: {
-        /*
-         * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
-         * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
-         */
         baseUrl: "SlightEdgeCoder.com",
-        // The protocol. This can be http or https.
         protocol: "https",
-        // Indicates whether the site is hosted on wordpress.com.
-        // If false, then the assumption is made that the site is self hosted.
-        // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
-        // If your site is hosted on wordpress.org, then set this to false.
         hostingWPCOM: false,
-        // If useACF is true, then the source plugin will try to import the Wordpress ACF Plugin contents.
-        // This feature is untested for sites hosted on wordpress.com.
-        // Defaults to true.
-        // useACF: true,
-        // Include specific ACF Option Pages that have a set post ID
-        // Regardless if an ID is set, the default options route will still be retrieved
-        // Must be using V3 of ACF to REST to include these routes
-        // Example: `["option_page_1", "option_page_2"]` will include the proper ACF option
-        // routes with the ID option_page_1 and option_page_2
-        // Dashes in IDs will be converted to underscores for use in GraphQL
-        // acfOptionPageIds: [],
-        // auth: {
-        //   // If hostingWPCOM is true then you will need to communicate with wordpress.com API
-        //   // in order to do that you need to create an app (of type Web) at https://developer.wordpress.com/apps/
-        //   // then add your clientId, clientSecret, username, and password here
-        //   // Learn about environment variables: https://www.gatsbyjs.org/docs/environment-variables
-        //   // If two-factor authentication is enabled then you need to create an Application-Specific Password,
-        //   // see https://en.support.wordpress.com/security/two-step-authentication/#application-specific-passwords
-        //   wpcom_app_clientSecret: process.env.WORDPRESS_CLIENT_SECRET,
-        //   wpcom_app_clientId: process.env.WORDPRESS_APP_CLIENTID,
-        //   wpcom_user: process.env.WORDPRESS_USER,
-        //   wpcom_pass: process.env.WORDPRESS_PASS,
-        // },
         // Set verboseOutput to true to display a verbose output on `npm run develop` or `npm run build`
         // It can help you debug specific API Endpoints problems.
         verboseOutput: false,
-        // Set how many pages are retrieved per API request.
-        // perPage: 300,
-        // Search and Replace Urls across WordPress content.
-        searchAndReplaceContentUrls: {
-          sourceUrl: "https://source-url.com",
-          replacementUrl: "https://replacement-url.com",
-        },
         // Set how many simultaneous requests are sent at once.
-        concurrentRequests: 10,
+        concurrentRequests: 5,
+        useACF: false,
         // Set WP REST API routes whitelists
         // and blacklists using glob patterns.
         // Defaults to whitelist the routes shown
