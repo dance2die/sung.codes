da9b70b02cca9ff196e8cadb3b97a50bffa64464
Sung Kim <sungmkim80@gmail.com>
2019-02-24 00:24:04 -0500

    Slug generated with decoded emoji works!!!!!!!

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,10 @@ class Home extends Component {
         <h1>Posts</h1>
         {data.allWordpressPost.edges.map(({ node }) => (
           <div css={{ marginBottom: rhythm(2) }} key={node.slug}>
-            <Link to={node.slug} css={{ textDecoration: `none` }}>
+            <Link
+              to={encodeURIComponent(node.slug)}
+              css={{ textDecoration: `none` }}
+            >
               <h3>{node.title}</h3>
             </Link>
             <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
