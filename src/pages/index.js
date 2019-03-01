fab650cd612ccd27eb050495a66b2be3c98a2bbb
Sung Kim <sungmkim80@gmail.com>
2019-02-28 21:34:23 -0500

    Converted 'Home' components in 'src/pages/index.js' to a Function Component from a Class Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React from "react"
 import { Link, graphql } from "gatsby"
 
 import Layout from "../layouts"
@@ -6,61 +6,53 @@ import PostIcons from "../components/post-icon"
 
 import { rhythm } from "../utils/typography"
 
-class Home extends Component {
-  render() {
-    const data = this.props.data
-    // const years = new Set(
-    //   data.allWordpressPost.edges.map(({ node }) => {
-    //     console.log(`node.year="${node.year}"`)
-    //     return node.year
-    //   })
-    // )
-    const years = new Set(
-      data.allWordpressPost.edges.map(({ node }) => node.year)
-    )
-    return (
-      <Layout>
-        <div css={{ marginBottom: rhythm(1) }}>
-          <h1>Pages</h1>
-          {data.allWordpressPage.edges.map(({ node }) => (
-            <div key={node.slug}>
-              <Link to={node.slug} css={{ textDecoration: `none` }}>
-                <h3>{node.title}</h3>
-              </Link>
-              <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-              <span>{node.date}</span>
-            </div>
-          ))}
-        </div>
-        <hr />
+function Home({ data }) {
+  const years = new Set(
+    data.allWordpressPost.edges.map(({ node }) => node.year)
+  )
 
-        <section>
-          <h1>Distinct Years</h1>
-          <ul>
-            {Array.from(years)
-              .sort((a, b) => b - a)
-              .map(year => (
-                <li key={year}>{year}</li>
-              ))}
-          </ul>
-        </section>
-
-        <h1>Posts</h1>
-        {data.allWordpressPost.edges.map(({ node }) => (
-          <div css={{ marginBottom: rhythm(2) }} key={node.slug}>
-            <Link
-              to={`${node.year}/${node.slug}`}
-              css={{ textDecoration: `none` }}
-            >
+  return (
+    <Layout>
+      <div css={{ marginBottom: rhythm(1) }}>
+        <h1>Pages</h1>
+        {data.allWordpressPage.edges.map(({ node }) => (
+          <div key={node.slug}>
+            <Link to={node.slug} css={{ textDecoration: `none` }}>
               <h3>{node.title}</h3>
             </Link>
             <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-            <PostIcons node={node} />
+            <span>{node.date}</span>
           </div>
         ))}
-      </Layout>
-    )
-  }
+      </div>
+      <hr />
+
+      <section>
+        <h1>Distinct Years</h1>
+        <ul>
+          {Array.from(years)
+            .sort((a, b) => b - a)
+            .map(year => (
+              <li key={year}>{year}</li>
+            ))}
+        </ul>
+      </section>
+
+      <h1>Posts</h1>
+      {data.allWordpressPost.edges.map(({ node }) => (
+        <div css={{ marginBottom: rhythm(2) }} key={node.slug}>
+          <Link
+            to={`${node.year}/${node.slug}`}
+            css={{ textDecoration: `none` }}
+          >
+            <h3>{node.title}</h3>
+          </Link>
+          <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+          <PostIcons node={node} />
+        </div>
+      ))}
+    </Layout>
+  )
 }
 
 export default Home
