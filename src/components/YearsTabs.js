a6f2eb55a9e382d04e92f5fac5025a39e325a8a2
Sung Kim <sungmkim80@gmail.com>
2019-03-01 21:10:45 -0500

    🛠  Added YearsTabs to the Layout

--- src/components/YearsTabs.js
+++ src/components/YearsTabs.js
@@ -1,7 +1,6 @@
 import React from "react"
 import produce from "immer"
 import classNames from "classnames"
-// import { graphql } from "gatsby"
 
 import "../styles/YearsTabs.scss"
 
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,7 @@
 // https://github.com/gatsbyjs/gatsby/blob/master/examples/using-wordpress/src/layouts/index.js
 import React from "react"
-import { Link } from "gatsby"
-// import PropTypes from "prop-types"
+import { Link, StaticQuery } from "gatsby"
+import YearsTabs from "../components/YearsTabs"
 
 import { rhythm, scale } from "../utils/typography"
 
@@ -11,55 +11,71 @@ const containerStyle = {
   padding: rhythm(3 / 4),
 }
 
-class DefaultLayout extends React.Component {
-  render() {
-    return (
-      <div>
-        <div
-          css={{
-            background: `rgb(207, 58, 62)`,
-            marginBottom: rhythm(1),
-            padding: `${rhythm(1)} 0px`,
-            "@media screen and (min-width: 500px)": {
-              padding: `${rhythm(2)} 0px`,
-            },
-          }}
-        >
-          <div css={containerStyle}>
-            <h1
-              css={{
-                margin: 0,
-                fontSize: scale(1.5).fontSize,
-                lineHeight: 1,
-                "@media screen and (min-width: 500px)": {
-                  fontSize: scale(1.9).fontSize,
-                  lineHeight: 1,
-                },
-              }}
-            >
-              <Link
-                css={{
-                  color: `rgb(224,203,144)`,
-                  ":hover": {
-                    color: `rgb(224,203,144)`,
-                    textDecoration: `none`,
-                  },
-                }}
-                to="/"
-              >
-                {"sung.{ codes }"}
-              </Link>
-            </h1>
-          </div>
-        </div>
-        <div css={containerStyle}>{this.props.children}</div>
-      </div>
-    )
-  }
+function getYears(edges) {
+  return Array.from(new Set(edges.map(({ node }) => node.year)))
 }
 
-// DefaultLayout.propTypes = {
-//   children: PropTypes.object.isRequired,
-// }
+function Layout(props) {
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          allWordpressPost(sort: { fields: [date], order: DESC }) {
+            edges {
+              node {
+                year: date(formatString: "YYYY")
+              }
+            }
+          }
+        }
+      `}
+      render={data => (
+        <div>
+          <div
+            css={{
+              background: `rgb(207, 58, 62)`,
+              marginBottom: rhythm(1),
+              padding: `${rhythm(1)} 0px`,
+              "@media screen and (min-width: 500px)": {
+                padding: `${rhythm(2)} 0px`,
+              },
+            }}
+          >
+            <div css={containerStyle}>
+              <h1
+                css={{
+                  margin: 0,
+                  fontSize: scale(1.5).fontSize,
+                  lineHeight: 1,
+                  "@media screen and (min-width: 500px)": {
+                    fontSize: scale(1.9).fontSize,
+                    lineHeight: 1,
+                  },
+                }}
+              >
+                <Link
+                  css={{
+                    color: `rgb(224,203,144)`,
+                    ":hover": {
+                      color: `rgb(224,203,144)`,
+                      textDecoration: `none`,
+                    },
+                  }}
+                  to="/"
+                >
+                  {"sung.{ codes }"}
+                </Link>
+              </h1>
+              <section>
+                <YearsTabs years={getYears(data.allWordpressPost.edges)} />
+              </section>
+            </div>
+          </div>
+          <div css={containerStyle}>{props.children}</div>
+        </div>
+      )}
+    />
+  )
+}
 
-export default DefaultLayout
+export default Layout
