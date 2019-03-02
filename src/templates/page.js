b9f6ccf4ce70cef0671a12e8fb833c9c393fbcf4
Sung Kim <sungmkim80@gmail.com>
2019-03-01 20:16:34 -0500

    ➕  Pages are correctly generated with post exerpts

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,44 +1,55 @@
 // https://github.com/gatsbyjs/gatsby/blob/master/examples/using-wordpress/src/templates/page.js
 import React from "react"
-import { graphql, StaticQuery } from "gatsby"
+import { graphql, Link } from "gatsby"
 import PostIcons from "../components/post-icon"
 import Layout from "../layouts"
 
 import { rhythm } from "../utils/typography"
 
-// const PageTemplate = ({ data }) => {
-const PageTemplate = ({ data }) => {
-  const currentPage = data.wordpressPage
-  console.log(`=> props`, currentPage)
+const PostExerpts = ({ posts }) => (
+  <>
+    {posts.map(({ node }) => (
+      <div css={{ marginBottom: rhythm(2) }} key={node.slug}>
+        <Link to={`${node.year}/${node.slug}`} css={{ textDecoration: `none` }}>
+          <h3>{node.title}</h3>
+        </Link>
+        <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+        <PostIcons node={node} />
+      </div>
+    ))}
+  </>
+)
 
-  const years = Array.from(
-    new Set(data.allWordpressPost.edges.map(({ node }) => node.year))
-  )
+const PageTemplate = ({ data }) => {
+  const currentPage = data.allWordpressPost
+  console.log(`=> props`, currentPage)
 
   return (
     <Layout>
       <h1 dangerouslySetInnerHTML={{ __html: currentPage.title }} />
       <PostIcons node={currentPage} css={{ marginBottom: rhythm(1 / 2) }} />
+      <PostExerpts posts={currentPage.edges} />
     </Layout>
   )
-
-  //   return (
-  //     <Layout>
-  //       <h1 dangerouslySetInnerHTML={{ __html: currentPage.title }} />
-  //       <PostIcons node={currentPage} css={{ marginBottom: rhythm(1 / 2) }} />
-  //       <div dangerouslySetInnerHTML={{ __html: currentPage.content }} />
-  //     </Layout>
-  //   )
 }
 
 export default PageTemplate
 
 export const pageQuery = graphql`
-  query {
-    allWordpressPost(sort: { fields: [date], order: DESC }) {
+  query($year: Int!) {
+    allWordpressPost(
+      sort: { fields: [date], order: DESC }
+      filter: { fields: { year: { eq: $year } } }
+    ) {
       edges {
         node {
-          year: date(formatString: "YYYY")
+          title
+          excerpt
+          slug
+          ...PostIcons
+          fields {
+            year
+          }
         }
       }
     }
