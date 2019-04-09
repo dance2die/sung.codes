9e826e66a755594b6fa3d0ad881f89621d9ceca2
Sung Kim <sungmkim80@gmail.com>
2019-04-09 11:20:46 -0400

    Removed categories & tags as they look noisy

--- src/components/post-icon.js
+++ src/components/post-icon.js
@@ -2,16 +2,48 @@ import { graphql } from "gatsby"
 import React from "react"
 import { rhythm } from "../utils/typography"
 
+const marginLeft = rhythm(1 / 5)
+
+// const tagCss = {
+//   color: "blue",
+//   fontSize: rhythm(1 / 2),
+//   marginLeft: marginLeft,
+// }
+
+// const categoryCss = {
+//   color: "red",
+//   fontSize: rhythm(1 / 2),
+//   marginLeft: marginLeft,
+// }
+
+const dateCss = {
+  color: "#000c",
+  fontSize: rhythm(1 / 1.75),
+  marginRight: rhythm(1),
+  fontStyle: "italic",
+}
+
 export default ({ node, className = `` }) => (
   <div css={{ marginTop: rhythm(-1 / 2) }} className={className}>
-    <span style={{ marginRight: rhythm(1) }}>{node.date}</span>
+    <span css={dateCss}>{node.date}</span>
+    {/* 
     {node.categories &&
       node.categories.map(category => (
-        <span style={{ marginRight: rhythm(1) }} key={category.name}>
+        <span
+          style={{ marginRight: rhythm(1) }}
+          key={category.name}
+          css={categoryCss}
+        >
           {category.name}
         </span>
       ))}
-    {node.tags && node.tags.map(tag => <span key={tag.name}>{tag.name}</span>)}
+    {node.tags &&
+      node.tags.map(tag => (
+        <span key={tag.name} css={tagCss}>
+          {tag.name}
+        </span>
+      ))}
+      */}
   </div>
 )
 
