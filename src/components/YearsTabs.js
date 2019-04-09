16b83597b590f2bd7f0e646584f2df203bd9ce6b
Sung Kim <sungmkim80@gmail.com>
2019-04-08 20:46:27 -0400

    Used rhythm for menu link padding

--- src/components/YearsTabs.js
+++ src/components/YearsTabs.js
@@ -2,6 +2,8 @@ import React from "react"
 import classNames from "classnames"
 import { Link } from "gatsby"
 
+import { rhythm } from "../utils/typography"
+
 import "../styles/YearsTabs.scss"
 
 // https://codesandbox.io/s/xkxj0rz74
@@ -19,7 +21,15 @@ function YearsTabs({ years }) {
           id={year}
           onClick={toggleClicked}
         >
-          <Link to={year} activeClassName="clicked">
+          <Link
+            css={{
+              // 1 rhythm is equal to the height of the line-height of
+              // normal body text.
+              padding: rhythm(1),
+            }}
+            to={year}
+            activeClassName="clicked"
+          >
             {year}
           </Link>
         </li>
