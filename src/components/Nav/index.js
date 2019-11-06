b8cf6b868cb03fd90a7ca1096e6288036430f265
Sung M. Kim <sungmkim80@gmail.com>
2019-11-05 23:30:35 -0500

    Moved heading title to Title

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,13 +1,19 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui"
 import { Header } from "theme-ui"
-import { Heading } from "@theme-ui/components"
+import { Heading, Flex } from "@theme-ui/components"
+
+const Title = () => (
+  <Flex>
+    <Heading as="h1">{"{ sung.codes }"}</Heading>
+    <Heading as="sup">by dance2die</Heading>
+  </Flex>
+)
 
 export default () => {
   return (
     <Header>
-      <Heading as="h1">{"{ sung.codes }"}</Heading>
-      <Heading as="sup">by dance2die</Heading>
+      <Title />
     </Header>
   )
 }
