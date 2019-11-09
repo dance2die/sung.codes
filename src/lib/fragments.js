a45e670ed2c557139f2995674dcd476c9fb48596
Sung M. Kim <sungmkim80@gmail.com>
2019-11-09 00:11:11 -0500

    Changed banner trace SVG color

--- src/lib/fragments.js
+++ src/lib/fragments.js
@@ -3,21 +3,21 @@ import { graphql } from "gatsby"
 export const bannerImage = graphql`
   fragment bannerImage260 on File {
     childImageSharp {
-      fluid(maxWidth: 260, traceSVG: { color: "#573ede" }, quality: 50) {
+      fluid(maxWidth: 260, traceSVG: { color: "#FFC600" }, quality: 50) {
         ...GatsbyImageSharpFluid_withWebp_tracedSVG
       }
     }
   }
   fragment bannerImage640 on File {
     childImageSharp {
-      fluid(maxWidth: 640, traceSVG: { color: "#573ede" }) {
+      fluid(maxWidth: 640, traceSVG: { color: "#FFC600" }) {
         ...GatsbyImageSharpFluid_withWebp_tracedSVG
       }
     }
   }
   fragment bannerImage720 on File {
     childImageSharp {
-      fluid(maxWidth: 720, traceSVG: { color: "#573ede" }, quality: 75) {
+      fluid(maxWidth: 720, traceSVG: { color: "#FFC600" }, quality: 75) {
         ...GatsbyImageSharpFluid_withWebp_tracedSVG
       }
     }
