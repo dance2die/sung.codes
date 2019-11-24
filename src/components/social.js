a7492ef46ed2eb75e2383136f5dea70b5b21b889
Sung M. Kim <sungmkim80@gmail.com>
2019-11-23 21:29:42 -0500

    Fixed RSS link color

--- src/components/social.js
+++ src/components/social.js
@@ -75,27 +75,31 @@ export const GitHub = ({ url = `${config.github}` }) => {
 export const RSS = ({
   color = `rgba(0, 0, 0, 0.7)`,
   url = `${config.rss}`,
-}) => (
-  <Link
-    to={url}
-    css={css`
-      color: ${color};
-      :hover {
-        color: rgba(0, 0, 0, 1);
-      }
-    `}
-    aria-label="Subscribe to my RSS Feed"
-  >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="23"
-      height="23"
-      viewBox="0 0 8 8"
+}) => {
+  const [colorMode] = useColorMode()
+
+  return (
+    <Link
+      to={url}
+      sx={{
+        "aria-label": "Subscribe to my RSS Feed",
+        color: theme => theme.colors.gray[3],
+        "&:hover": {
+          color: getHoverColor(colorMode === "default"),
+        },
+      }}
     >
-      <rect width="8" height="8" rx="1.5" fill="currentColor" />
-      <circle cx="2" cy="6" r="1" />
-      <path d="m 1,4 a 3,3 0 0 1 3,3 h 1 a 4,4 0 0 0 -4,-4 z" />
-      <path d="m 1,2 a 5,5 0 0 1 5,5 h 1 a 6,6 0 0 0 -6,-6 z" />
-    </svg>
-  </Link>
-)
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="23"
+        height="23"
+        viewBox="0 0 8 8"
+      >
+        <rect width="8" height="8" rx="1.5" fill="currentColor" />
+        <circle cx="2" cy="6" r="1" />
+        <path d="m 1,4 a 3,3 0 0 1 3,3 h 1 a 4,4 0 0 0 -4,-4 z" />
+        <path d="m 1,2 a 5,5 0 0 1 5,5 h 1 a 6,6 0 0 0 -6,-6 z" />
+      </svg>
+    </Link>
+  )
+}
