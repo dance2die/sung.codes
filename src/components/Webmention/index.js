51e6c301fc0dc661b2c94f4e479408a5faf74d74
Sung M. Kim <sungmkim80@gmail.com>
2020-02-16 15:56:20 -0500

    Shows webmentions, using swyx's webmention as a test #32

--- src/components/Webmention/index.js
+++ src/components/Webmention/index.js
@@ -1,8 +1,11 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui"
-import { Heading } from "@theme-ui/components"
+import { Heading, Button, Text, Box, Flex, Image } from "@theme-ui/components"
 // eslint-disable-next-line
 import React, { useEffect, useState } from "react"
+import Dangerous from "dangerous-components"
+
+import ExternalLink from "#components/Link/ExternalLink"
 
 const initialCounts = {
   count: 0,
@@ -62,17 +65,111 @@ function WebmentionCount({ target }) {
   )
 }
 
+function Replies({ replies }) {
+  const replyElements = replies.map(link => (
+    <li sx={{ margin: "1.6rem 0" }}>
+      <Flex direcition="row">
+        <ExternalLink
+          to={link.data.author.url}
+          sx={{ flexShrink: 0, cursor: "pointer" }}
+        >
+          <Image
+            sx={{ borderRadius: "50%" }}
+            width={40}
+            src={link.data.author.photo}
+            alt={`avatar of ${link.data.author.name}`}
+          />
+        </ExternalLink>
+        <Dangerous
+          sx={{ padding: "0 1rem 0" }}
+          html={link.activity.sentence_html}
+          as="article"
+        />
+      </Flex>
+    </li>
+  ))
+
+  return (
+    <Box my={[2, 2, 3, 3]}>
+      {replies && replies.length ? (
+        <ul
+          sx={{
+            listStyle: "none",
+          }}
+        >
+          {replyElements}
+        </ul>
+      ) : (
+        <Text>There is no reply...</Text>
+      )}
+    </Box>
+  )
+}
+
 function WebmentionReplies({ target }) {
+  const [page, setPage] = useState(0)
+  const [fetchState, setFetchState] = useState("fetching")
+  const [replies, setReplies] = useState([])
+  const perPage = 30
+
+  const getMentions = () =>
+    fetch(
+      // `https://webmention.io/api/mentions?page=${page}&per-page=20&sort-by=published&target=${target}`,
+      // `https://webmention.io/api/mentions?page=${page}&per-page=50&target=${target}/` // trailing slash impt
+      `https://webmention.io/api/mentions?page=${page}&per-page=${perPage}&target=https://www.swyx.io/writing/clientside-webmentions/`
+    )
+      .then(response => response.json())
+      .then(json => json.links.filter(x => x.activity.type !== "like"))
+
+  const incrementPage = () => setPage(previousPage => previousPage + 1)
+  const fetchMore = () => {
+    getMentions()
+      .then(returnedReplies => {
+        if (returnedReplies.length) {
+          setReplies([...replies, ...returnedReplies])
+        } else {
+          setFetchState("nomore")
+        }
+      })
+      .then(incrementPage)
+  }
+
+  useEffect(() => {
+    getMentions().then(returnedReplies => {
+      console.info(`page=${page} => returnedReplies???`, returnedReplies)
+      setReplies(returnedReplies)
+      setFetchState("done")
+    })
+  }, [target])
+
   return (
     <>
-      <h1>Replies</h1>
+      {fetchState === "fetching" && <Text>Fetching Replies...</Text>}
+      <Replies replies={replies} />
+      {fetchState !== "nomore" ? (
+        <Button
+          mt={[2, 2, 3, 3]}
+          sx={{ cursor: "pointer" }}
+          onClick={fetchMore}
+        >
+          Fetch More...
+        </Button>
+      ) : (
+        <Text>
+          No further replies found.{" "}
+          <ExternalLink
+            to={`https://twitter.com/intent/tweet/?text=My%20thoughts%20on%20${target}`}
+          >
+            Tweet about this post
+          </ExternalLink>{" "}
+          and it will show up here!
+        </Text>
+      )}
     </>
   )
 }
 
 function Webmention({ target }) {
-  const [replies, setReplies] = useState()
-
   return (
     <>
       <Heading as="h2" mb={[2, 2, 3, 3]}>
