fcf25fe8310b24ec5ca75ab9ce087f4f350b29e2
Sung M. Kim <sungmkim80@gmail.com>
2020-02-17 11:16:54 -0500

    Wrapped 'Webmention' with error boundary

--- src/components/Webmention/index.js
+++ src/components/Webmention/index.js
@@ -4,6 +4,7 @@ import { Heading, Button, Text, Box, Flex, Image } from "@theme-ui/components"
 // eslint-disable-next-line
 import React, { useEffect, useState, useCallback } from "react"
 import Dangerous from "dangerous-components"
+import ErrorBoundary from "react-error-boundary"
 
 import ExternalLink from "#components/Link/ExternalLink"
 
@@ -190,15 +191,30 @@ function WebmentionReplies({ target }) {
   )
 }
 
+const WebmentionFallbackComponent = ({ componentStack, error }) => (
+  <Box>
+    <Text>
+      <strong>Error occurred while fetching Webmention...</strong>
+    </Text>
+    <Text>Here’s what we know…</Text>
+    <Text>
+      <strong>Error:</strong> {error.toString()}
+    </Text>
+    <Text>
+      <strong>Stacktrace:</strong> {componentStack}
+    </Text>
+  </Box>
+)
+
 function Webmention({ target }) {
   return (
-    <>
+    <ErrorBoundary FallbackComponent={WebmentionFallbackComponent}>
       <Heading as="h2" mb={[2, 2, 3, 3]}>
         Webmentions
       </Heading>
       <WebmentionCount target={target} />
       <WebmentionReplies target={target} />
-    </>
+    </ErrorBoundary>
   )
 }
 
